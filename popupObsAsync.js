f5e0fa2a76d90c6caf4baf515b06b13e66efa899
Will Howard <34873242+fairlycasual@users.noreply.github.com>
2018-02-21 21:28:08 -0800

    Update popupObsAsync.js

--- popupObsAsync.js
+++ popupObsAsync.js
@@ -4,7 +4,7 @@ async function getObs() {
     .catch(error => console.log('ipinfo.io error: ' + error));
     console.log(info.loc);
 
-  var obs = await fetch('https://api.mesowest.net/v2/stations/nearesttime?&radius='${info.loc}',100&limit=1&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf&vars=air_temp,wind_speed')
+  var obs = await fetch(`https://api.mesowest.net/v2/stations/nearesttime?&radius=${info.loc},100&limit=1&units=ENGLISH&token=804c1eb09a394255beef8e84b68123bf&vars=air_temp,wind_speed`)
   .then(resp => resp.json())
   .catch(error => console.log('MesoWest request Error! Network Error?' + error));
 
